b86d35a2e6d20f58884652a03812ee067cc1e5ca
PhamVanDu <phamvandu1m78@gmail.com>
2026-09-14 21:52:16 +0700

    Test

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { queryClient } from './config/queryClient' // Import từ file cấu hình riêng
+import { queryClient } from './config/queryClient'
 import { QueryClientProvider } from '@tanstack/react-query'
 
 createRoot(document.getElementById('root')!).render(
